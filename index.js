1bcaad11af6aa0aed4f68b10f6818f51bdef6bbc
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-11-26 20:35:40 +0100

    Creando un Master Page en Handlebars

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const app = express();
 // Habilitar handlebars como view
 // Para que funcione, se debe crear una carpeta llamada views y dentro otra llamada layouts
 // Como hemos indicado que el defaultLayout se llamará layout, dentro de la carpeta layouts habrá
-// que crear el archivo layout.handlebars
+// que crear el archivo layout.hbs porque hemos dicho que el extension name es hbs. Sino se pone eso
+// se deberá llamar layout.handlebars
 app.engine(
-  'handlebars',
+  '.hbs',
   engine({
+    extname: '.hbs',
     defaultLayout: 'layout',
   })
 );
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
