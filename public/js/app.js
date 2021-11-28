557f064ae0e374456b728d3cdfe3ffe675fbf617
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-11-28 07:05:51 +0100

    Guardando un Set con los Skills para la vacante

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-alert('hola');
+document.addEventListener('DOMContentLoaded', () => {
+  const skills = document.querySelector('.lista-conocimientos');
+
+  if (skills) {
+    skills.addEventListener('click', agregarSkills);
+  }
+});
+
+// Por fuera de agregarSkills para que sólo se cree una vez
+const skills = new Set();
+
+const agregarSkills = (e) => {
+  if (e.target.tagName === 'LI') {
+    if (e.target.classList.contains('activo')) {
+      // quitarlo del set y quitar la clase
+      skills.delete(e.target.textContent);
+      e.target.classList.remove('activo');
+    } else {
+      // agregarlo al set y agregar la clase
+      skills.add(e.target.textContent);
+      e.target.classList.add('activo');
+    }
+  }
+
+  const skillsArray = [...skills];
+  document.querySelector('#skills').value = skillsArray;
+};
